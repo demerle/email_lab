9d09e5c2e433d2dbb25f7b9fe388c8e9cf41751d
demerle <danny.emerle@gmail.com>
2026-09-23 22:58:47 -0400

    Created basic views/ui

--- app/src/main/java/com/example/email_lab/MainActivity.kt
+++ app/src/main/java/com/example/email_lab/MainActivity.kt
@@ -5,8 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var emails: List<Email>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Lookup the RecyclerView in activity layout
+        val emailsRv = findViewById<RecyclerView>(R.id.emailsRv)
+        // Fetch emails
+        emails = EmailFetcher.getEmails()
+        // Create adapter passing in the list of emails
+        val adapter = EmailAdapter(emails)
+        // Attach the adapter to the RecyclerView to populate items
+        emailsRv.adapter = adapter
+        // Set layout manager to position the items
+        emailsRv.layoutManager = LinearLayoutManager(this)
     }
 }
